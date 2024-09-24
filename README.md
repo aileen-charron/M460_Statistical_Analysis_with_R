2ee2c4c04930c215e8cc9e475508f619ddbdf505
aileen-charron <acharron1@bryant.edu>
2024-09-24 10:23:13 -0400

    Base R Notes

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 Assignment1: [Assignment1](file:///C:/Users/student/Documents/M461-Statistical-Analysis-with-R/Assignment1.html)
 
+Assignment2: [Assignment2]
---
+++ in_class_codes.R
@@ -0,0 +1,10 @@
+# read in a csv file
+df <-  read.csv('https://bryantstats.github.io/math421/data/WHO-COVID-19-global-data.csv')
+
+
+# show the names of the columns
+names(df)
+
+# show the structure of the data
+str(df)
+
